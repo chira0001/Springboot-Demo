dca1e1d076b52fb820333b75dfdad706fce7969c
Chirath <chirathwijesinghe123@gmail.com>
2026-03-06 15:18:14 +0530

    Updates

--- src/main/java/com/demo/demo/controller/RestuarentController.java
+++ src/main/java/com/demo/demo/controller/RestuarentController.java
@@ -18,15 +18,16 @@ public class RestuarentController {
 
     @GetMapping("/")
     public String viewHomePage(Model model){
-        model.addAttribute("listRestuarents",restuarentService.getAllRestuarent());
-        return "index";
+        model.addAttribute("listRestuarents", restuarentService.getAllRestuarent());
+        return "index"; // => templates/index.html
     }
 
     @GetMapping("/new")
     public String showNewForm(Model model){
-        model.addAttribute("restuarent",new Restuarent());
-        return "new_restuarent";
+        model.addAttribute("restuarent", new Restuarent());
+        return "new_restuarent"; // => templates/new_restuarent.html
     }
+
     @PostMapping("/save")
     public String saveRestuarent(@ModelAttribute("restuarent") Restuarent restuarent){
         restuarentService.saveRestuarent(restuarent);
@@ -36,7 +37,7 @@ public class RestuarentController {
     @GetMapping("/edit/{id}")
     public String showEditForm(@PathVariable Integer id, Model model){
         model.addAttribute("restuarent", restuarentService.getRestuarentById(id));
-                return "edit";
+        return "edit"; // => templates/edit.html
     }
 
     @GetMapping("/delete/{id}")
